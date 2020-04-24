733353f9c44d52515cc944519a624f5d57daa03b
Rafał Powalski <rafal.powalski@applica.ai>
2020-04-24 21:55:03 +0200

    add glue generation for xlm

--- fairseq/models/roberta/model_xlmr.py
+++ fairseq/models/roberta/model_xlmr.py
@@ -6,7 +6,7 @@
 Unsupervised Cross-lingual Representation Learning at Scale
 """
 
-from fairseq.models import register_model
+from fairseq.models import register_model, register_model_architecture
 
 from .hub_interface import RobertaHubInterface
 from .model import RobertaModel
@@ -35,3 +35,17 @@ class XLMRModel(RobertaModel):
             **kwargs,
         )
         return RobertaHubInterface(x['args'], x['task'], x['models'][0])
+
+
+@register_model_architecture('xlmr', 'xlmr_base')
+def xlm_architecture(args):
+    base_architecture(args)
+
+
+@register_model_architecture('xlmr', 'xlmr_large')
+def xlm_architecture(args):
+    args.encoder_layers = getattr(args, 'encoder_layers', 24)
+    args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 1024)
+    args.encoder_ffn_embed_dim = getattr(args, 'encoder_ffn_embed_dim', 4096)
+    args.encoder_attention_heads = getattr(args, 'encoder_attention_heads', 16)
+    base_architecture(args)
